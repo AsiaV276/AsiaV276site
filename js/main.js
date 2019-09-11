9a195873b3aa714925651729ecb474f805941289
Asia Valentine <asiavalentine22@gmail.com>
2019-09-11 18:30:23 -0400

    first git commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+﻿// Your code here!
+document.getElementById("img-alexa").addEventListener("click", showDetails);
+
+function showDetails() {
+    var alexaDetails = "These are the project details!";
+    document.getElementById("proj-text").textContent = alexaDetails;
+}
+
+function changeOpacity() {
+    document.getElementsByClassName("portfolio-item").style.opacity = "0.75";
+}
